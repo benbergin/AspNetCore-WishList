478b81aadce2db5ac28b946040241a7482a193d7
benbergin <benbergin@gmail.com>
2019-03-07 09:45:30 -0700

    Added DBContext

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; /* needed for the attributes listed in brackets below */
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
